3993e7bb3dfbd44edbcb3ada3a26b9475b75cb16
Divor <todjropatrice2002@gmail.com>
2026-07-03 09:35:20 +0000

    refactor(esport): remove dead join-request model/endpoints and isRecruiting/lookingFor (superseded by recruitment campaigns)

--- src/esport/esport.module.ts
+++ src/esport/esport.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EsportController } from './esport.controller';
 import { EsportService } from './esport.service';
-import { CommunityModule } from '../community/community.module';
 
 @Module({
-  imports: [CommunityModule],
   controllers: [EsportController],
   providers: [EsportService],
   exports: [EsportService],
